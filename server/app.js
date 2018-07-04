60310d41449c4960aa801538f8e552023d86ca29
jneuhaus20 <jneuhaus20@yahoo.com>
2018-07-04 15:38:40 +0000

    removed old pearl router refs

--- server/app.js
+++ server/app.js
@@ -2,14 +2,11 @@ const express = require('express')
 const morgan = require('./log.js')
 const bodyParser = require('body-parser')
 
-const pearlRouter = require('./pearl-router.js')
-
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.text());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(morgan);
-app.use('/pearls', pearlRouter);
 
 app.listen(3000, () => console.log('Launched Dev Web API on port 3000'));
