c9a95609a5055d859cb60e18e46807f50237c432
John Neuhaus <jneuhaus20@yahoo.com>
2018-08-24 22:40:26 -0500

    Added mongodb connector and switched to using that

--- data/effective-engineer/init.js
+++
@@ -1,7 +0,0 @@
-var data = require('./effective-engineer.json');
-
-module.exports = function(app) {
-	// Load attribution
-	var attrModel = app.models.Attribution;
-	attrModel.create(data.attribution);
-}
--- server/datasources.js
+++ server/datasources.js
@@ -1,8 +1,13 @@
-{
+module.exports = {
   "in-mem-db": {
     "name": "in-mem-db",
     "localStorage": "",
     "file": "",
     "connector": "memory"
+  },
+
+  "mongodb": {
+    "name": "mongodb",
+    "url": process.env.MONGODB_URI
   }
 }
