e0b766b4b4d0fc28c2de70ebe78432a9cc6feb0e
John Neuhaus <jneuhaus20@yahoo.com>
2018-09-01 00:05:14 -0500

    Updated npm packages and fixed mongodb connection

--- server/boot/init-data.js
+++ server/boot/init-data.js
@@ -2,13 +2,16 @@
 const data = require('../../data/effective-engineer/effective-engineer.json');
 
 module.exports = app => {
+    console.log("MONGODB_URI:", process.env.MONGODB_URI);
+    
     console.log('Importing initial data to in-mem db...');
 
     const attrModel = app.models.Attribution;
     const attr = data.attribution;
     
     console.log('Importing attribution...');
-    attrModel.create(attr, (err, attrObj) => {
+    attrModel.upsert(attr, (err, attrObj) => {
+        console.log(err);
         console.log('... Success! Created attribution: ', attrObj);
 
         const pearlModel = app.models.Pearl;
@@ -18,7 +21,7 @@ module.exports = app => {
         .map(pearl => ({...pearl, attributionId: attrObj.id}))
         .forEach(pearl => {
             console.log('Importing a pearl...');
-            pearlModel.create(pearl, (err, obj) => console.log(err || '... Success!'));
+            pearlModel.upsert(pearl, (err, obj) => console.log(err || '... Success!'));
         });
     });
 
