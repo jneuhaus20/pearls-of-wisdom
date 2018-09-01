936323e38ac38d5e6c8c88c59e42c5f1c4d48313
John Neuhaus <jneuhaus20@yahoo.com>
2018-09-01 01:57:04 -0500

    Added remote method for 'random'

--- common/models/pearl.js
+++ common/models/pearl.js
@@ -2,4 +2,29 @@
 
 module.exports = function(Pearl) {
 
+    Pearl.random = function() {
+        const promise = new Promise((resolve, reject) => {
+
+            const pearlCollection = Pearl.getDataSource().connector.collection(Pearl.modelName);
+            pearlCollection.aggregate({
+                $sample: {
+                    size: 1
+                }
+            }, function (err, groupByRecords) {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(groupByRecords.next());
+                }
+            });
+
+        });
+
+        return promise;
+    }
+
+    Pearl.remoteMethod('random', {
+        description: 'Retrieves a random Pearl',
+        returns: {arg: 'pearl', type: Pearl}
+    });
 };
